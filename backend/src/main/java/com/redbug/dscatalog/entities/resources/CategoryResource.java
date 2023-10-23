8cac40631d21ac2e4750300d01883574fcbaaa7b
BugRed <o.adrian.rodrigues@gmail.com>
2023-10-23 20:29:28 -0300

    POST new category implements

--- backend/src/main/java/com/redbug/dscatalog/entities/resources/CategoryResource.java
+++ backend/src/main/java/com/redbug/dscatalog/entities/resources/CategoryResource.java
@@ -1,14 +1,18 @@
 package com.redbug.dscatalog.entities.resources;
 
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.redbug.dscatalog.DTO.CategoryDTO;
 import com.redbug.dscatalog.services.CategoryService;
@@ -33,4 +37,13 @@ public class CategoryResource {
         CategoryDTO dto = service.findById(id);
         return ResponseEntity.ok().body(dto);
     }
+
+    @PostMapping()
+    public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto){
+
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
+    } 
 }
