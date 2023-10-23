ece824f48c7aaa0e0387685ef4910dea7d724013
BugRed <o.adrian.rodrigues@gmail.com>
2023-10-23 18:48:49 -0300

    add category DTO

--- backend/src/main/java/com/redbug/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/redbug/dscatalog/services/CategoryService.java
@@ -1,10 +1,13 @@
 package com.redbug.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.redbug.dscatalog.DTO.CategoryDTO;
 import com.redbug.dscatalog.entities.Category;
 import com.redbug.dscatalog.entities.repositories.CategoryRepository;
 
@@ -13,9 +16,12 @@ public class CategoryService {
 
     @Autowired
     private CategoryRepository repository;
-    
+
     @Transactional(readOnly = true)
-    public List<Category> findAll(){
-        return  repository.findAll();
+    public List<CategoryDTO> findAll() {
+        
+        List<Category> list = repository.findAll();
+        return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+
     }
 }
